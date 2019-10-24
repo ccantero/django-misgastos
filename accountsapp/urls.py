2fe7ccc53895f860c45c0a57d1578d0493cddd1f
Cristhian Cantero <cantero.solutions@gmail.com>
2019-10-24 02:17:43 -0300

    Heroku

--- accountsapp/urls.py
+++ accountsapp/urls.py
@@ -6,6 +6,7 @@ app_name = 'accountsapp'
 
 urlpatterns = [
 	path('login/',auth_views.LoginView.as_view(template_name='accountsapp/login.html'), name='login'),
+	path('change-password/',auth_views.PasswordChangeView.as_view(template_name='accountsapp/change-password.html', success_url='/'), name='change_password'),
 	path('logout/',auth_views.LogoutView.as_view(), name='logout'),
 	path('signup/',views.SignUpView.as_view(), name='signup'),
 ]
