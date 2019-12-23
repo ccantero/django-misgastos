dff7438e8ec1e0485abe53546d17369da6a9b20c
Cristhian Cantero <cantero.solutions@gmail.com>
2019-12-22 23:28:54 -0300

    Disable DEBUG - Fix 500 #10

--- misgastos/settings.py
+++ misgastos/settings.py
@@ -142,7 +142,7 @@ if os.path.isfile(dotenv_file):
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # For Heroku
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # For Heroku
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
 LOGIN_REDIRECT_URL = 'home'
