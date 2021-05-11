01810874a3385cd43182f25f08c7b68a947df3f9
ccantero <cantero.solutions@gmail.com>
2021-05-11 00:23:29 -0300

    Fix max_lenght chat_id

--- telegramapi/models.py
+++ telegramapi/models.py
@@ -5,5 +5,5 @@ from django.utils import timezone
 
 class TelegramMessage(models.Model):
 	message = models.TextField()
-	chat_id = models.CharField(max_length=100)
+	chat_id = models.CharField(max_length=255)
 	create_date = models.DateTimeField(default=timezone.now)
